98741037b783100b156ee0e90f3e3025266830eb
RonMankivsky <87318489+RonMankivsky@users.noreply.github.com>
2022-01-02 13:29:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+import os
 a = 5
 while True:
     print("loop")
+
+
+print("bad ip's from: https://lists.blocklist.de/lists/all.txt")
+print("1.15.121.25")
+print("17791257")
+print("0x010f7919")
+print("116.23.174")
